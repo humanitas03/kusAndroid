5d74811c55a3887281aecf00089be4eb6cdb96a7
humanitas03 <dlswp113@gmail.com>
2016-09-07 16:52:10 +0900

    /*version 1.0*/
    /*
     * KUSITMS 13th Webview for Android
     * 2016. 09. 07
     */

---
+++ app/src/main/java/dlswp113/com/kwebview/LaunchActivity.java
@@ -0,0 +1,39 @@
+package dlswp113.com.kwebview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by USER on 2016-04-28.
+ */
+public class LaunchActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        Handler h = new Handler();
+
+        h.postDelayed(new splashhandler(), 3000);
+//        h.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//             Intent i = new Intent(LaunchActivity.this, MainActivity.class);
+//                startActivity(i);
+//                finish(); }   }, 3000);
+
+    }
+
+    class splashhandler implements Runnable {
+        public void run(){
+
+            startActivity(
+                    new Intent(getApplication(), MainActivity.class));
+            LaunchActivity.this.finish();
+        }
+    }
+}
---
+++ app/src/main/java/dlswp113/com/kwebview/MainActivity.java
@@ -0,0 +1,42 @@
+package dlswp113.com.kwebview;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+//android Web view를 이용한 KUSITM 어플리케이션
+public class MainActivity extends AppCompatActivity {
+
+    private static String user_id = "";
+    private GCMManager gcmManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TelephonyManager telManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String phoneNum = telManager.getLine1Number();
+
+        user_id = phoneNum;
+        //registerGcm();
+        gcmManager = new GCMManager(this, user_id);
+        //gcmManager.sendUserPush(user_id, "test_title", "test_contents");
+
+        String url = "http://dlswp113.dothome.co.kr/kusitms13/index.html"; //kusitm web address
+        Log.i("URL", "Opening URL :" + url);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient()); // 이걸 안해주면 새창이 뜸
+        webView.loadUrl(url);
+    }
+
+}
+
+
